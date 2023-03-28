0f0809507760cd9e2bee2c3ba792367b7ea258e8
Ibrahim <ibraguim.karsamov@gmail.com>
2023-03-28 16:56:53 +0200

    script update

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const userPassword = document.querySelector('#user_password');
-const userPasswordConfirm = document.querySelector('#user_password_confirm');
+const userPassword = document.getElementById("user_password");
+const userPasswordConfirm = document.getElementById("user_password_confirm");
+const form = document.querySelector("form");
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (userPasswordConfirm.value != userPassword.value) {
+        console.log("Passwords do not match.");
+        userPassword.setCustomValidity("Passwords do not match.");
+    }
+
+    userPassword.reportValidity();
+})
